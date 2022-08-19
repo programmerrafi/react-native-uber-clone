685cbc179be12927175af907793efea4bd5f94de
Md Rafi <mdrafiprogrammer@gmail.com>
2022-08-19 21:00:21 +0600

    React-Native-Uber-clone start

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import { View, Text, Image, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import SafeAreaWrapper from "../configs/SafeAreaWrapper";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
-  // useLayoutEffect(() => {
-  //   navigation.setOptions({
-  //     headerShown: false,
-  //   });
-  // }, []);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaWrapper>
+      <View>
+        <Text>Home Screen</Text>
+      </View>
+    </SafeAreaWrapper>
   );
 };
 
