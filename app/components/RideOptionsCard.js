b1b4338a8a72761127d1abfaf1a9ab7eb3812fe2
Md Rafi <mdrafiprogrammer@gmail.com>
2022-08-22 20:50:24 +0600

    React-native uber is running

--- app/components/RideOptionsCard.js
+++ app/components/RideOptionsCard.js
@@ -12,6 +12,7 @@ import {
 import { Icon } from "react-native-elements";
 import Screen from "./Screen";
 import { useSelector } from "react-redux";
+import { selectOrigin, selectDestination } from "../Redux/slice/navSlice";
 
 const data = [
   {
@@ -39,26 +40,38 @@ const SEARCH_CHARGE_RATE = 1.5;
 const RideOptionsCard = () => {
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
-  const travelTimeInformation = useSelector(selectTravelTimeInformation);
+  // const travelTimeInformation = useSelector(selectTravelTimeInformation);
   const origin = useSelector(selectOrigin);
   const destination = useSelector(selectDestination);
 
-  //   useEffect(() =>{
-  //     if(!origin || !destination) navigation.push('NavigateCard')
-  // }, [origin, destination])
+  useEffect(() => {
+    if (!origin || !destination) navigation.push("NavigateCard");
+  }, [origin, destination]);
 
-  // const travelConst = (item) => {
-  //     return ((travelTimeInformation?.duration?.value * SEARCH_CHARGE_RATE * item?.multiplier) / 100).toFixed(2)
-  // }
+  const travelConst = (item) => {
+    return (
+      (travelTimeInformation?.duration?.value *
+        SEARCH_CHARGE_RATE *
+        item?.multiplier) /
+      100
+    ).toFixed(2);
+  };
 
-  // const onChoose = () =>{
-  //     if(!selected) return Alert.alert('Please select a ride option')
-  //     navigation.push('SuccessScreen', { data: {...selected, distance: travelTimeInformation?.distance?.text, time: travelTimeInformation?.duration.text, price: travelConst(selected)} })
-  // }
+  const onChoose = () => {
+    if (!selected) return Alert.alert("Please select a ride option");
+    navigation.push("SuccessScreen", {
+      data: {
+        ...selected,
+        distance: travelTimeInformation?.distance?.text,
+        time: travelTimeInformation?.duration.text,
+        price: travelConst(selected),
+      },
+    });
+  };
 
   return (
-    <Screen style={tailwind`bg-white h-full`}>
-      <View style={tailwind`items-center flex-row justify-center mb-3`}>
+    <Screen className={`bg-white h-full`}>
+      <View className={`items-center flex-row justify-center mb-3`}>
         <TouchableOpacity
           style={{ left: 10, position: "absolute", zIndex: 100 }}
           onPress={() => navigation.push("NavigateCard")}
@@ -68,37 +81,35 @@ const RideOptionsCard = () => {
             name="arrowleft"
             color="black"
             size={23}
-            style={tailwind`p-3`}
+            className={`p-3`}
           />
         </TouchableOpacity>
-        <Text style={tailwind`text-center text-xl font-bold`}>
+        <Text className={`text-center text-xl font-bold`}>
           Select a ride - {travelTimeInformation?.distance?.text}
         </Text>
       </View>
-      <View style={tailwind`flex-1 mt-2`}>
+      <View className={`flex-1 mt-2`}>
         <FlatList
           data={data}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
             <TouchableOpacity
-              style={tailwind`flex-row items-center justify-between px-5 ${
+              className={`flex-row items-center justify-between px-5 ${
                 selected?.id === item.id && "bg-gray-100"
               }`}
               onPress={() => setSelected(item)}
             >
               <Image source={{ uri: item.image }} style={styles.image} />
-              <View
-                style={tailwind`flex-row items-center justify-between flex-1`}
-              >
+              <View className={`flex-row items-center justify-between flex-1`}>
                 <View>
-                  <Text style={tailwind`text-xl font-bold text-black`}>
+                  <Text className={`text-xl font-bold text-black`}>
                     {item.title}
                   </Text>
-                  <Text style={tailwind`text-gray-600`}>
+                  <Text className={`text-gray-600`}>
                     {travelTimeInformation?.duration?.text} Travel time
                   </Text>
                 </View>
-                <Text style={tailwind`text-gray-800 text-lg`}>
+                <Text className={`text-gray-800 text-lg`}>
                   {/* {new Intl.NumberFormat('en-us', {
                                         style: 'currency',
                                         currency: 'USD'
@@ -114,13 +125,13 @@ const RideOptionsCard = () => {
       </View>
       <View>
         <TouchableOpacity
-          style={tailwind`bg-black py-3 m-3 rounded-lg ${
+          className={`bg-black py-3 m-3 rounded-lg ${
             !selected && "bg-gray-300"
           }`}
           disabled={!selected}
           onPress={onChoose}
         >
-          <Text style={tailwind`text-center text-white text-xl`}>
+          <Text className={`text-center text-white text-xl`}>
             Choose {selected?.title}
           </Text>
         </TouchableOpacity>
